ab3c965c0224ce70bdc19b946ac2ddfcfaa35d71
Fickos <filip.volaric99@gmail.com>
2024-01-29 00:32:34 +0100

    Github pages setup in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://Fickos.github.io',
+  base: '/portfolio'
 });
